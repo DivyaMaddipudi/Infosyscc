d95160b07e01803165daad077ac982230efa412e
SatyaDivya <divya.maddipudi1@gmail.com>
2019-02-13 16:28:23 +0530

    modified word_scrambling project

--- word_scrambling/main.py
+++ word_scrambling/main.py
@@ -11,21 +11,14 @@ def scramble_word(word):
     return first + "".join(mid) + last
 
 def scramble_text(text):
-    '''li = [',','.','!',';']
     for i in text:
-        if i in li:
-            return scramble_word(i)'''
-    return re.sub(r"\w+", lambda match: scramble_word(match.group()),text)
+        return re.sub(r"\w+", lambda match: scramble_word(match.group()),text)
 
 def scramble_file(input_filename):
     with open(input_filename) as input_file:
         text = input_file.read()
 
     scrambled_text = scramble_text(text)
-
-    '''with open(output_filename) as output_file:
-        output_file.write(scrambled_text)'''
-
     f = open(r"E:\Git Folders\Infosyscc\word_scrambling\after_scramble.txt", "w")
     f.write(scrambled_text)
 
